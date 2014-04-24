d8e3c1a78792a1727319d9308963f907a1d8f6ed
CraigChilds94 <childscraig17@gmail.com>
2014-04-24 09:01:08 +0100

    Uploaded initial game stuff

---
+++ Assets/Scripts/AnimationHandle.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimationHandle : MonoBehaviour {
+
+    public Animator anim;
+    public CharacterMotor motor;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (!motor.grounded)
+        {
+            if (Input.GetButton("Jump"))
+            {
+                anim.SetBool("jumping", true);
+            }
+        }
+        else
+        {
+            anim.SetBool("jumping", false);
+        }
+
+        anim.SetFloat("speed", Mathf.Abs(motor.movement.velocity.x));
+	}
+}
